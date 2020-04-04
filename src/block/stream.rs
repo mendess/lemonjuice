b74d94b9f494b7ff8e45a7e103e7bf139b494dd8
mendess <pedro.mendes.26@gmail.com>
2020-04-04 13:11:06 +0100

    Compiles again

--- src/block/stream.rs
+++ src/block/stream.rs
@@ -1,15 +1,16 @@
-use async_std::{task, stream::{self, Stream, StreamExt}};
-use super::{Block,Config};
-use crate::text::Text;
+// use async_std::{task, stream::{self, Stream}};
+// use futures_util::StreamExt;
+// use super::{Block,Config};
+// use crate::text::Text;
 
-pub fn make_block_stream(config: Config) -> impl Stream<Item = Text> {
-    stream::empty()
-}
+// pub fn make_block_stream(config: Config) -> impl Stream<Item = Text> {
+//     stream::empty()
+// }
 
-pub fn block_to_stream(block: Block) -> impl Stream<Item = Text> {
-    stream::repeat(()).then(|| async {
-        task::sleep(block.interval).await;
-        block.update();
-        block.to_text(0)
-    })
-}
+// pub fn block_to_stream(mut block: Block) -> impl Stream<Item = Option<Text>> {
+//     stream::repeat(()).then(move |_| async {
+//         task::sleep(block.interval).await;
+//         block.update();
+//         block.to_text(0)
+//     })
+// }
